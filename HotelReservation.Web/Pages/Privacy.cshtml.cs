1d3b84f6a4f4e389861f7723118a1c55ceffe4a8
Eray Ertac <erayertac84@gmail.com>
2025-11-26 18:59:45 +0300

    Initial commit

---
+++ HotelReservation.Web/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelReservation.Web.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
